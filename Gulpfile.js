b6849b571f54db1fff066cbcd5d53ecb5ed6e627
Pamplemousse <xavier.maso@net-c.com>
2015-07-21 19:35:22 +1200

    Make live-reload work

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-
 var gls = require('gulp-live-server');
+
 gulp.task('serve', function() {
   var server = gls.new('app.js', 8082, {env: {NODE_ENV: 'development'}});
   server.start();
@@ -12,6 +12,8 @@ gulp.task('serve', function() {
       'public/javascripts/*.js',
       'public/stylesheets/*.css'
     ],
-    server.start
+    function() {
+      server.notify.apply(server, arguments);
+    }
   );
 });
