7a0db3e42cde155d4c33a328c4ecd1bb112440fa
Pamplemousse <xavier.maso@net-c.com>
2015-07-21 20:26:37 +1200

    Add nsp to the project
      * check npm packages for security vulnerabilities
      * add watch tasks:
          - to generate shrinwrap file on package.json changes
          - to call nsp on the generated shrinkwrap
      * update shrinkwrap file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var gulpNSP = require('gulp-nsp');
+var shrinkwrap = require('gulp-shrinkwrap');
+
+gulp.task('nsp', function (cb) {
+  gulpNSP('./npm-shrinkwrap.json', cb);
+});
+
+gulp.task('shrinkwrap', function () {
+  return gulp.src('package.json')
+    .pipe(shrinkwrap())
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('serve', function() {
   var server = gls.new('app.js', 8082, {env: {NODE_ENV: 'development'}});
@@ -18,4 +30,9 @@ gulp.task('serve', function() {
   );
 });
 
-gulp.task('default', ['serve']);
+gulp.task('watch', function() {
+  gulp.watch('package.json', ['shrinkwrap']);
+  gulp.watch('npm-shrinkwrap.json', ['nsp']);
+});
+
+gulp.task('default', ['serve', 'watch']);
